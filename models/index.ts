3ac96956ae808841aa8218b76793dab67c248237
Mayureshju <juvekarmayuresh37@gmail.com>
2026-02-23 17:44:24 +0530

    feat: add models index file and import necessary models

---
+++ models/index.ts
@@ -0,0 +1,10 @@
+import './Department';
+import './User';
+import './RoleDefinition';
+import './SOP';
+import './SOPVersion';
+import './SOPComment';
+import './EditRequest';
+import './AuditLog';
+
+export {};
